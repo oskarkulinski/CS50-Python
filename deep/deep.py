727a93285d601062d34467c19f9382534edcd3fb
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2022-07-13 14:44:37 +0000

    Wed, Jul 13, 2022, 2:44 PM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,5 +1,5 @@
 answer = input('What is the answer to the Great Question of Life? ')
-answer = answer.lower()
+answer = answer.lower().strip()
 if answer=='42' or answer == 'forty two' or answer == 'forty-two':
     print('Yes')
 else:
