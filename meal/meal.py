a47b945b633a18e07bc387dadf05fa58933c03c7
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2022-07-13 17:14:08 +0000

    Wed, Jul 13, 2022, 5:14 PM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,9 +1,13 @@
 def main():
     hour = input('What time is it? ')
     convert(hour)
-    if 7 < hour < 8:
+    if 7 <= hour <= 8:
         print('breakfast time')
-    
+    elif 12 <= hour <= 13:
+        print('lunch time')
+    elif 18 <= hour <=19:
+        print('dinner time')
+
 
 def convert(time):
     hr, min = time.split(sep=':')
@@ -12,5 +16,4 @@ def convert(time):
     time = hr + min
 
 
-if __name__ == "__main__":
-    main()
+main()
