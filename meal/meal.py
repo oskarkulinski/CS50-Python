afbad487488d44bae0fdb416f66805a52f2f169d
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2022-07-13 17:03:14 +0000

    Wed, Jul 13, 2022, 5:03 PM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,9 +1,13 @@
 def main():
-    ...
-
+    hour = input('What time is it? ')
+    if
 
 def convert(time):
-    
+    hr, min = time.split(sep=':')
+    hr = float(hr)
+    min = float(min)/60
+    time = hr + min
+
 
 if __name__ == "__main__":
     main()
