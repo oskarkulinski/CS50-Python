4ab8039b424b82404839f277d2c1dc4b40a50100
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2022-07-13 15:24:22 +0000

    Wed, Jul 13, 2022, 3:24 PM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,3 +1,17 @@
 name = input('What is the name of your file? ')
 name = name.strip().lower()
-match 
+type = name.
+
+match name:
+    case '.gif':
+        print('image/gif')
+    case '.jpg' or '.jpeg':
+        print('image/jpeg')
+    case  '.png':
+        print('image/png')
+    case '.pdf':
+        print('application/pdf')
+    case '.txt':
+        print('text/plain')
+    case '.zip':
+        print('application/zip')
