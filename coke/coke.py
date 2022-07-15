344e830b06d40c93778f7fdc7f99ceed0baa404d
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2022-07-15 11:34:27 +0000

    Fri, Jul 15, 2022, 11:34 AM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -3,7 +3,7 @@ while x > 0:
     coin = int(input('How much did you put in? '))
     if coin == 25 or coin == 10 or coin == 5:
         x = x - coin
-    if x>0:
+    if x >= 0:
         print('Amount due:', x)
     if x <0:
         print('Your change is: ', -x)
