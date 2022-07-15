fb17ca5fe5e0f4db44bd79c2ffaef148ca2d1b21
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2022-07-15 12:31:11 +0000

    Fri, Jul 15, 2022, 12:31 PM +00:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,11 +1,7 @@
 text = input('What do you want to say? ')
 y = len(text) - 1
-x = 0
-while x <= y:
-    if text[x]=='a' or text[x]=='i' or text[x]!='e' or text[x]!='o' or text[x]!='u':
-        print(text[x], end = '')
-    else:
-        print('')
+for x in text:
+    if text[x]!='a' or text[x]!='i' or text[x]!='e' or text[x]!='o' or text[x]!='u':
+        print(x, end = '')
 
-    x= x + 1
 print('')
