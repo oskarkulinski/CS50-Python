74a8bcc9351a9c3253285ab8b5853a83d8ea70cc
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2022-07-15 12:05:04 +0000

    Fri, Jul 15, 2022, 12:05 PM +00:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -3,6 +3,5 @@ x = len(text) - 1
 
 while x >0:
     if text[x]=='a' or text[x]=='i' or text[x]=='e' or text[x]=='a' or text[x]=='o' or text[x]=='u':
-        text[x]= ''
+        print(textx]= ''
     x= x -1
-print(text)
