371ae86eb96b0610b68d914a2676a131ddfdc0d1
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2022-10-01 12:15:42 +0000

    ddd, Oct 10/1/2022, YYYY, 12:15 A +0

--- deep/deep.py
+++ deep/deep.py
@@ -1,4 +1,4 @@
-answer = input('What is the answer to the Great Question of Life? ')
+Kanswer = input('What is the answer to the Great Question of Life? ')
 answer = answer.lower().strip()
 if answer=='42' or answer == 'forty two' or answer == 'forty-two':
     print('Yes')
