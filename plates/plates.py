3371ce584c136750f9c64d291dc31d229c9c4a80
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-09 19:35:00 +0000

    ddd, Sep 9/9/2023, YYYY, 7:35 A +0

---
+++ plates/plates.py
@@ -0,0 +1,15 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if s.len() < 2 or s.len() > 6:
+        return False
+    else if 
+
+
+main()
