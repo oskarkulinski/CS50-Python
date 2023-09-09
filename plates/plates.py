ce27b773e7e883a9714a71c63809d2ec3f345174
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-09 19:47:44 +0000

    ddd, Sep 9/9/2023, YYYY, 7:47 A +0

--- plates/plates.py
+++ plates/plates.py
@@ -12,7 +12,7 @@ def is_valid(s):
         return False
     else:
         shortened = s.lstrip(string.ascii_letters)
-        if len(shortened) + 2 > len(s):
+        if len(shortened) + 2 > len(s) and len(shortened) < len(s):
             return False
         else:
             if shortened.isdigit() and shortened[0] != '0':
