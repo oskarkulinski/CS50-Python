45291a35b72cd3ac7021c125d8a62c40e0ee4047
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-09 19:27:41 +0000

    ddd, Sep 9/9/2023, YYYY, 7:27 A +0

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,6 +1,6 @@
 text = input('What do you want to say? ')
 for x in text:
-    if x =='a' or x=='e' or x == 'i' or x == 'o' or x == 'u':
+    if x =='a' or x=='e' or x == 'i' or x == 'o' or x == 'u' or x=='A' or x=='E' or x=='I' or x == 'O' or x == 'U':
         continue
     else:
         print(x, end = '')
