f39fbe73bd791dbddf929239969fc6af9f8f462e
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-09 19:16:16 +0000

    ddd, Sep 9/9/2023, YYYY, 7:16 A +0

--- camel/camel.py
+++ camel/camel.py
@@ -1,14 +1,9 @@
 name = input('What is the name of your variable? ')
 answer = ""
-y = 0
 for x in name:
     if x.islower() == True:
-        answer[y] = x
-        y += 1
+        print(x, end="")
     else:
-        answer[y] = '_'
-        answer[y + 1] = str(x -26)
-        y += 2
+        print("_", x.lower, end="")
 
-
-print (answer)
+print()
