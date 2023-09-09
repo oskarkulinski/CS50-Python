4c8aced8b9cd61ee90139cf857426f0b14780e86
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-09 19:44:11 +0000

    ddd, Sep 9/9/2023, YYYY, 7:44 A +0

--- plates/plates.py
+++ plates/plates.py
@@ -1,3 +1,4 @@
+import string
 def main():
     plate = input("Plate: ")
     if is_valid(plate):
@@ -10,7 +11,14 @@ def is_valid(s):
     if s.len() < 2 or s.len() > 6:
         return False
     else:
-        s.lstrip()
+        shortened = s.lstrip(string.ascii_letters)
+        if shortened.len() + 2 > s.len():
+            return False
+        else:
+            if shortened.isdigit():
+                return True
+            else:
+                return False
 
 
 main()
