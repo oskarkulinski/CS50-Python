44d5f75cf1df3344329465690283886dc56222c8
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-15 12:09:11 +0000

    Fri, Sep 15, 2023, 2:09 PM +02:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,15 +1,17 @@
 while True:
-        frac = input("Fraction: ")
-        x = 
+    frac = input("Fraction: ")
+    tup = frac.partition("/")
     try:
-        x = int(input("Fraction: ")) * 100
-    except ValueError:
+        x = int(tup[0])
+        y = int(tup[2])
+        res = x/y
+    except ValueError, ZeroDevisionError:
         pass
     else:
         break
-if x >= 99:
+if res >= 99:
     print("F")
-elif x <= 1:
+elif res <= 1:
     print("E")
 else:
     print(x)
