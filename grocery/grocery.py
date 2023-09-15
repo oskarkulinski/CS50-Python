1511b768d67bb25981a1f4cb12fd11dbbfee7ab7
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-15 16:13:28 +0000

    Fri, Sep 15, 2023, 6:13 PM +02:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -5,7 +5,7 @@ while True:
         item = input()
     except EOFError:
         print()
-        groceries = sort
+        groceries = sorted(groceries)
         for thing in groceries:
             print(f"{groceries[thing]} {thing}")
         break
