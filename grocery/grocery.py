95790bd680c44a3be5b146ddc7c4c5499d6adb39
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-15 15:14:48 +0000

    Fri, Sep 15, 2023, 5:14 PM +02:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -4,12 +4,12 @@ while True:
     try:
         item = input("Item: ")
     except EOFError:
+        for thing in groceries
+            print(f"")
         break
     item = item.upper()
     try:
         total += menu[item]
     except KeyError:
-        pass
-    else:
-        print("Total: ${x:.2f}".format(x = total))
+        groceries[item] = 0
 print()
