f773d389d8374dd129f26745d97c12b1a03ad85f
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-16 19:56:22 +0000

    Sat, Sep 16, 2023, 9:56 PM +02:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -2,17 +2,17 @@ names = []
 n = 0
 while True:
     try:
-        names[n] = input("Name: ")
+        names.append(input("Name: "))
     except EOFError:
         break
     else:
         n += 1
-
+print()
 print("Adieu, adieu, to ", end="")
 print(names[0], end = "")
 if n > 1:
     for name in names[1:-1]:
-        print(",", name)
-    print("and ", names[len(names) - 1], end="")
+        print(",", name,sep="", end="")
+    print(" and", names[len(names) - 1])
 
 
