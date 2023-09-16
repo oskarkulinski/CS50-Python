926130a478222169269f9545083dc9286494a0d3
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-16 20:19:17 +0000

    Sat, Sep 16, 2023, 10:19 PM +02:00

--- game/game.py
+++ game/game.py
@@ -4,11 +4,17 @@ level = 0
 guess = 0
 
 while level <= 0:
-    level = int(input("Level: "))
+    try:
+        level = int(input("Level: "))
+    except ValueError:
+        continue
 
 answer = random.randint(1, level)
 while guess != answer:
-    guess = int(input("Guess: "))
+    try:
+        guess = int(input("Guess: "))
+    except ValueError:
+        continue
     if guess < answer:
         print("Too small!")
     elif guess > answer:
