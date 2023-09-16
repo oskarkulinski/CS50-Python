5894a4039adbe7c678fd9cd3f5c4680899182267
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-16 19:34:49 +0000

    Sat, Sep 16, 2023, 9:34 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -4,15 +4,15 @@ import random
 
 if len(sys.argv) == 2:
     if sys.argv[1] != "-f" and sys.argv[1] != "--font":
-        sys.exit()
+        sys.exit("Wrong first argument")
     try:
         f = pyfiglet.Figlet(font=sys.argv[2])
     except ValueError:
-        sys.exit()
+        sys.exit("This font does not exist")
 elif len(sys.argv) == 0:
     f = pyfiglet.Figlet(font=random.choice(pyfiglet.Figlet().getFonts()))
 else:
-    sys.exit()
+    sys.exit("Wrong amount of arguments")
 
 text = input("Input: ")
 print("Output: ")
