3e3177efb513d5a9e617bb7b706b8d0a830b6478
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-17 10:05:55 +0000

    Sun, Sep 17, 2023, 12:05 PM +02:00

--- professor/professor.py
+++ professor/professor.py
@@ -4,11 +4,11 @@ import random
 def main():
     n = get_level()
     correct = 0
-    for i in range(1,10):
+    for i in range(0,10):
         x = generate_integer(n)
         y = generate_integer(n)
         answer = x + y
-        for _ in range(1, 3):
+        for _ in range(0, 3):
             try:
                 guess = int(input(f"{x} + {y} = "))
             except ValueError:
@@ -18,6 +18,8 @@ def main():
                 if guess == answer:
                     correct += 1
                     break
+                else:
+                    print("EEE")
     print("Score:", correct)
 
 def get_level():
