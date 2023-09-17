2f4f92d86d6fc2281637a7eaad91fcee2942c6f1
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-17 10:54:53 +0000

    Sun, Sep 17, 2023, 12:54 PM +02:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -2,6 +2,13 @@ import sys
 import requests
 
 try:
+    n = float(sys.argv[1])
+except IndexError:
+    sys.exit("Missing command-line argument")
+except ValueError:
+    sys.exit("Command-line argument is not a number")
+
+try:
     ...
 except requests.RequestException:
     ...
