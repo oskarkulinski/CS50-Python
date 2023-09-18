c5222e698dd0a6290a2937ac6d9cea0fb33c5707
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-18 13:48:55 +0000

    Mon, Sep 18, 2023, 3:48 PM +02:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -4,17 +4,29 @@ def main():
     test_is_valid_short()
     test_is_valid_long()
 
-def test_is_valid_short()
+def test_is_valid_short():
     assert is_valid("aa") == False
     assert is_valid("AA") == False
     assert is_valid("AAaaAA") == True
     assert is_valid("aAAa") == True
 
-def test_is_valid_long()
-    assert is_valid("aaaaaaa")
-    assert is_valid("AaaaaaA")
+def test_is_valid_long():
+    assert is_valid("aaaaaaa") == False
+    assert is_valid("AaaaaaA") == False
+    assert is_valid("aaaaaa") == True
 
-def test_is_valid_letters()
+def test_is_valid_letters():
+    assert is_valid("aa1111") == True
+
+    assert is_valid("a1111") == False
+    assert is_valid("11") == False
+    assert is_valid("aaa1") == True
+    assert is_valid("aa1a1") == False
+    assert is_valid("aa,11") == False
+
+def test_is_valid_numbers():
+    assert is_valid("aa111a") == False
 
 
-def test_is_valid_numbers()
+if __name__ == "__main__":
+    main()
