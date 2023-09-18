7f591f10dd799597e171943b076e05192c974d18
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-18 12:42:45 +0000

    Mon, Sep 18, 2023, 2:42 PM +02:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,11 +1,23 @@
+from twttr import shorten
+
 def main():
+    test_shorten_no_vowels()
 
-def test_shorten_lowercase:
+def test_shorten_lowercase():
+    assert shorten("twitter") == "twttr"
+    assert shorten("england") == "nglnd"
+    assert shorten("table") == "tbl"
 
-    
 
-def test_shorten_uppercase:
+def test_shorten_uppercase():
+    assert shorten("TWITTER") == "TWTTR"
+    assert shorten("ENGLAND") == "NGLND"
+    assert shorten("TABLE") == "TBL"
 
+def test_shorten_no_vowels():
+    assert shorten("twttr") == "twttr"
+    assert shorten("") == ""
+    assert shorten("qwrtypsdfghjklzxcvbnm") == "qwrtypsdfghjklzxcvbnm"
 
 
 if __name__ == "__main__":
--- test_twttr/twttr.py
+++ test_twttr/twttr.py
@@ -5,11 +5,11 @@ def main():
 
 def shorten(word):
     shortened = ""
-    for x in text:
-        if x =='a' or x=='e' or x == 'i' or x == 'o' or x == 'u' or x=='A' or x=='E' or x=='I' or x == 'O' or x == 'U':
+    for c in word:
+        if c =='a' or c =='e' or c == 'i' or c == 'o' or c == 'u' or c =='A' or c =='E' or c =='I' or c == 'O' or c == 'U':
             continue
         else:
-            shortened += x
+            shortened += c
 
 
 if __name__ == "__main__":
