163144115820d18876d009a2977dd5a3b2e62b43
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-18 14:15:10 +0000

    Mon, Sep 18, 2023, 4:15 PM +02:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,7 +1,23 @@
-import fuel
+from fuel import convert
+from fuel import gauge
 
 def main():
+    test_conver_valid()
+    test_conver_error()
+    test_gauge()
+
+
+def test_convert_valid()
+    
+
+
+def test_convert_error()
 
 
 
-def 
+def test_gauge()
+
+
+
+if __name__ == "__main__":
+    main()
