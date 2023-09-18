a8b7cd4111bb411e4807007824713c6cf7094dc6
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-18 14:04:43 +0000

    Mon, Sep 18, 2023, 4:04 PM +02:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -1,9 +1,20 @@
 def main():
-    ...
+    fraction = input("Fraction: ")
+    percentage = convert(fraction)
+    print(gauge(percentage))
 
 
 def convert(fraction):
-    ...
+    values = frac.partition("/")
+    try:
+        x = int(tup[0])
+        y = int(tup[2])
+        res = int(round(x * 100 / y, 0))
+    except (ValueError, ZeroDivisionError):
+        pass
+    else:
+        if res <= 100:
+            break
 
 
 def gauge(percentage):
