abf2c1113d3eabd9fb74afe5ca56d8812e4fe39b
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-18 13:08:49 +0000

    Mon, Sep 18, 2023, 3:08 PM +02:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -5,13 +5,22 @@ def main():
     test_value_uppercase()
 
 def test_value_lowercase():
-
+    assert value("hello") == 0
+    assert value("hey") == 20
+    assert value("good morning") == 100
+    assert value("hello, sir") == 0
 
 def test_value_uppercase():
-
+    assert value("Hello") == 0
+    assert value("Hey") == 20
+    assert value("Hood morning") == 100
+    assert value("Hello, sir") == 0
 
 def test_value_blank():
+    assert value(" hello") == 0
+    assert value("  hey") == 20
+    assert value("   good morning") == 100
+    assert value("    hello, sir") == 0
 
-
-if __name__ == "__main__"
+if __name__ == "__main__":
     main()
