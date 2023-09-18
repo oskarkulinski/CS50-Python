afc361e6fd618d348d7867197b828edfca4a12a6
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-18 12:44:30 +0000

    Mon, Sep 18, 2023, 2:44 PM +02:00

--- test_twttr/twttr.py
+++ test_twttr/twttr.py
@@ -9,8 +9,8 @@ def shorten(word):
         if c =='a' or c =='e' or c == 'i' or c == 'o' or c == 'u' or c =='A' or c =='E' or c =='I' or c == 'O' or c == 'U':
             continue
         else:
-            shortened += c
-
+            shortened = shortened + c
+    return shortened
 
 if __name__ == "__main__":
     main()
