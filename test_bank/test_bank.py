d6052dd61dea845a2f328a518ea7e26617436149
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-18 13:03:41 +0000

    Mon, Sep 18, 2023, 3:03 PM +02:00

---
+++ test_bank/test_bank.py
@@ -0,0 +1,17 @@
+from bank import value
+
+def main():
+    test_value_lowercase()
+    test_value_uppercase()
+
+def test_value_lowercase():
+
+
+def test_value_uppercase():
+
+
+def test_value_blank():
+
+
+if __name__ == "__main__"
+    main()
