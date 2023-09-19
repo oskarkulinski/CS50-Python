b1348b14d1c64ecf3159e91170d8f67897882f4b
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-19 12:31:15 +0000

    Tue, Sep 19, 2023, 2:31 PM +02:00

---
+++ shirt/shirt.py
@@ -0,0 +1,22 @@
+import sys
+
+if len(sys.argv) < 3:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 3:
+    sys.exit("Too many command-line arguments")
+else:
+    fileName = sys.argv[1].split(".")
+    try:
+        if fileName[1] != "jpg" or fileName[1] != "jpeg" or fileName[1] != "png":
+            sys.exit("Invalid output")
+    except IndexError:
+        sys.exit("Invalid output")
+    else:
+        outName = sys.argv[1].split(".")
+        if fileName[1] != outName[1]:
+            sys.exit("Input and output have different extensions")
+    try:
+        fileIn = open(sys.argv[1], "r")
+    except FileNotFoundError:
+        sys.exit("Input does not exist")
+    
