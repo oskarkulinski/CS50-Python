847fc94f158d5227894b674a9e99d929613afd9d
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-21 12:42:24 +0000

    Thu, Sep 21, 2023, 2:42 PM +02:00

--- working/working.py
+++ working/working.py
@@ -7,7 +7,7 @@ def main():
 
 
 def convert(s):
-    if hours := re.search(r"(\d):?(\d\d)? ([AP]M) to (\d):?(\d\d)? ([AP]M)",s.strip()):
+    if hours := re.search(r"(\d{1,2}):?(\d\d)? ([AP]M) to (\d{1,2}):?(\d\d)? ([AP]M)",s.strip()):
         if int(hours.group(1)) > 12 or int(hours.group(4)) > 12:
             raise ValueError
         if hours.group(2) != None and hours.group(5) != None:
