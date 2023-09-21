52a4175ff83bdbe8914af8d3945ad024be09ba8f
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-21 11:17:31 +0000

    Thu, Sep 21, 2023, 1:17 PM +02:00

--- working/working.py
+++ working/working.py
@@ -14,40 +14,38 @@ def convert(s):
             if int(hours.group(2)) > 60 or int(hours.group(5)) > 60:
                 raise ValueError
             else:
-                result = ""
-
                 #start time
 
-                if hours.group(3) == "AM" and int(hours.group(1)) < 10:
-                    result = "0" + hours.group(1)
-                elif hours.group(3) == "PM":
-                    result = str(int(hours.group(1)) + 12)
-                else:
-                    result = hours.group(1)
+                result = convert_hour(1, hours)
                 result += ":" + hours.group(2) + " " + hours.group(3)
 
                 result += " to "
 
                 #finish time
 
-                if hours.group(6) == "AM" and int(hours.group(4)) < 10:
-                    result = "0" + hours.group(4)
-                elif hours.group(6) == "PM":
-                    result = str(int(hours.group(4)) + 12)
-                else:
-                    result = hours.group(4)
-                result += ":" + hours.group(3) + " " + hours.group(6)
+                result += convert_hour(4, hours)
+                result += ":" + hours.group(5) + " " + hours.group(6)
 
                 return result
         else:
-            
-
+            result = convert_hour(1, hours)
+            result += ":00 " + hours.group(3)
+            result += " to "
+            result += convert_hour(4, hours)
+            result += ": " + hours.group(6)
 
     else:
         raise ValueError
 
 
-...
+def convert_hour(x, hours):
+    if hours.group(x + 2) == "AM" and int(hours.group(x)) < 10:
+        result = "0" + hours.group(x)
+    elif hours.group(x + 2) == "PM":
+            result = str(int(hours.group(x)) + 12)
+    else:
+        result = hours.group(x)
+    return result
 
 
 if __name__ == "__main__":
