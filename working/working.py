5eb5188240e0ae3f7d6423008ffc0643a5fb7d5f
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-21 11:09:55 +0000

    Thu, Sep 21, 2023, 1:09 PM +02:00

--- working/working.py
+++ working/working.py
@@ -15,11 +15,34 @@ def convert(s):
                 raise ValueError
             else:
                 result = ""
+
+                #start time
+
                 if hours.group(3) == "AM" and int(hours.group(1)) < 10:
                     result = "0" + hours.group(1)
+                elif hours.group(3) == "PM":
+                    result = str(int(hours.group(1)) + 12)
                 else:
                     result = hours.group(1)
                 result += ":" + hours.group(2) + " " + hours.group(3)
+
+                result += " to "
+
+                #finish time
+
+                if hours.group(6) == "AM" and int(hours.group(4)) < 10:
+                    result = "0" + hours.group(4)
+                elif hours.group(6) == "PM":
+                    result = str(int(hours.group(4)) + 12)
+                else:
+                    result = hours.group(4)
+                result += ":" + hours.group(3) + " " + hours.group(6)
+
+                return result
+        else:
+            
+
+
     else:
         raise ValueError
 
