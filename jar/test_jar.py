cd621eb6a885acb4c93a8e37c8c5b470b484a5ac
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-22 19:50:54 +0000

    Fri, Sep 22, 2023, 9:50 PM +02:00

--- jar/test_jar.py
+++ jar/test_jar.py
@@ -1,11 +1,46 @@
-import jar
-
+from jar import Jar
+import pytest
 
 def test_jar_general():
     j1 = Jar()
-    print(j1)
-    print(str(j1.capacity))
-    print(str(j1.size))
+    assert print(j1) == ""
+    assert print(str(j1.capacity)) == "12"
+    assert print(str(j1.size)) == "0"
 
 def test_jar_deposit():
-    assert 
+    j1 = Jar()
+    j1.deposit(10)
+    assert print(j1) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
+    assert print(str(j1.capacity)) == "12"
+    assert print(str(j1.size)) == "10"
+    with pytest.raises(ValueError):
+        j1.deposit(5)
+
+def test_jar_withdraw():
+    j1 = Jar()
+    j1.deposit(10)
+    j1.withdraw(5)
+    assert print(j1) == "🍪🍪🍪🍪🍪"
+    assert print(str(j1.capacity)) == "12"
+    assert print(str(j1.size)) == "5"
+    with pytest.raises(ValueError):
+        j1.withdraw(10)
+
+
+def test_jar_other():
+    j1 = Jar()
+    j1.deposit(10)
+    j1.withdraw(5)
+    assert print(j1) == "🍪🍪🍪🍪🍪"
+    assert print(str(j1.capacity)) == "12"
+    assert print(str(j1.size)) == "5"
+
+    j1.size = 9
+    j1.capacity = 10
+
+    assert print(j1) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪"
+    assert print(str(j1.capacity)) == "10"
+    assert print(str(j1.size)) == "9"
+    with pytest.raises(ValueError):
+        j1.withdraw(10)
+        j1.deposit(5)
