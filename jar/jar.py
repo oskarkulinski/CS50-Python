ef473733f72f3ff2bf8782c46ad6debcbe773865
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-22 19:18:17 +0000

    Fri, Sep 22, 2023, 9:18 PM +02:00

--- jar/jar.py
+++ jar/jar.py
@@ -3,7 +3,11 @@
 
 class Jar:
     def __init__(self, capacity=12):
-        self.capacity 
+        if int(capacity) < 0:
+            raise ValueError("Capacity can't be negative")
+        else:
+            self.capacity = capacity
+            self.size = 0
 
     def __str__(self):
         ...
