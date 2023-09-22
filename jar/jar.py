582e02c43f51eb94b0cf27c6ada3b099dd54667e
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-22 19:16:29 +0000

    Fri, Sep 22, 2023, 9:16 PM +02:00

---
+++ jar/jar.py
@@ -0,0 +1,23 @@
+
+
+
+class Jar:
+    def __init__(self, capacity=12):
+        self.capacity 
+
+    def __str__(self):
+        ...
+
+    def deposit(self, n):
+        ...
+
+    def withdraw(self, n):
+        ...
+
+    @property
+    def capacity(self):
+        ...
+
+    @property
+    def size(self):
+        ...
