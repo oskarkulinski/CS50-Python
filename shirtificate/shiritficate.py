26af421de01ad75000677ccd27e5d336a998c409
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-23 10:19:30 +0000

    Sat, Sep 23, 2023, 12:19 PM +02:00

--- shirtificate/shiritficate.py
+++ shirtificate/shiritficate.py
@@ -2,15 +2,16 @@ from fpdf import FPDF
 
 def main():
     name = input("Name: ")
-    make_shirt()
+    make_shirt(name)
 
 
-def make_shirt():
+def make_shirt(name):
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("ZapfDingbats", "", 16)
-    pdf.cell(60, 10, 'CS50 Shirtificate!', align="C")
-    
+    pdf.cell(60, 10, "CS50 Shirtificate!", align="C")
+    pdf.image("shirtificate.png", 40, 50, 33)
+    pdf.cell(60, 10, f"{name.capitalize()} took CS50", align="C")
 
 if __name__ == "__main__":
     main()
