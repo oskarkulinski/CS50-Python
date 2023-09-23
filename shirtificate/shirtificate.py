a6017d2c9bb4a8c0cac2edb4664af006c4ea4cda
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-23 10:40:35 +0000

    Sat, Sep 23, 2023, 12:40 PM +02:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -13,8 +13,9 @@ def make_shirt(name):
     pdf.cell(200, 20, "CS50 Shirtificate", align="C")
     pdf.image("shirtificate.png", 10, 40, 190)
     pdf.ln(100)
-    with pdf.set_text_color()
-    pdf.cell(200, 10, f"{name.lower().title()} took CS50", align="C")
+    pdf.set_text_color(255, 255, 255)
+    pdf.set_font("Times", "I", 30)
+    pdf.cell(200, 20, f"{name.lower().title()} took CS50", align="C")
     pdf.output("shirtificate.pdf")
 
 if __name__ == "__main__":
