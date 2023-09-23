7b7aa510a26988f73ca9e27d363e69c5e32f68e8
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-23 10:12:40 +0000

    Sat, Sep 23, 2023, 12:12 PM +02:00

--- shirtificate/shiritficate.py
+++ shirtificate/shiritficate.py
@@ -1,10 +1,14 @@
-import fpdf2
+from fpdf import FPDF
 
 def main():
     name = input("Name: ")
-    
+    make_shirt()
 
 
+def make_shirt():
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font()
 
 
 if __name__ == "__main__":
