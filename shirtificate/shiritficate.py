009f1cdb45de188b64a2a9ac7df38da9a2caacec
oskarkulinski <109166833+oskarkulinski@users.noreply.github.com>
2023-09-23 10:15:40 +0000

    Sat, Sep 23, 2023, 12:15 PM +02:00

--- shirtificate/shiritficate.py
+++ shirtificate/shiritficate.py
@@ -9,8 +9,8 @@ def make_shirt():
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("ZapfDingbats", "", 16)
-    pdf.cell(60, 10, 'CS50 Shirtificate!', 1)
-
+    pdf.cell(60, 10, 'CS50 Shirtificate!', align="C")
+    
 
 if __name__ == "__main__":
     main()
